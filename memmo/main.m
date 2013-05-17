6dee74a32c0210a53cdef5f338af3f73765f8cdc
Mostfa Essam <mostfa@dsldevice.lan>
2013-05-17 11:11:51 +0300

    Initial Commit

---
+++ memmo/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  memmo
+//
+//  Created by Mostfa Essam on 5/17/13.
+//  Copyright (c) 2013 com.hello. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
